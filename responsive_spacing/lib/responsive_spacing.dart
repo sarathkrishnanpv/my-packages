a4d6dbb1a40c5325d1f0cca8830969667594532c
sarath.krishnan1 <sarath7916@gmail.com>
2025-02-05 17:48:54 +0530

    initial commit

---
+++ responsive_spacing/lib/responsive_spacing.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+
+class Oneh extends StatelessWidget {
+  const Oneh({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(height: MediaQuery.of(context).size.height * .01);
+  }
+}
+
+class Halfh extends StatelessWidget {
+  const Halfh({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(height: MediaQuery.of(context).size.height * .005);
+  }
+}
+
+class Twoh extends StatelessWidget {
+  const Twoh({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(height: MediaQuery.of(context).size.height * .02);
+  }
+}
+
+class Threeh extends StatelessWidget {
+  const Threeh({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(height: MediaQuery.of(context).size.height * .03);
+  }
+}
+
+class Fiveh extends StatelessWidget {
+  const Fiveh({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(height: MediaQuery.of(context).size.height * .05);
+  }
+}
+
+class Sevenh extends StatelessWidget {
+  const Sevenh({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(height: MediaQuery.of(context).size.height * .07);
+  }
+}
+
+class Onew extends StatelessWidget {
+  const Onew({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(width: MediaQuery.of(context).size.width * .01);
+  }
+}
+
+class Twow extends StatelessWidget {
+  const Twow({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(width: MediaQuery.of(context).size.width * .02);
+  }
+}
+
+class Threew extends StatelessWidget {
+  const Threew({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(width: MediaQuery.of(context).size.width * .03);
+  }
+}
+
+class Fivew extends StatelessWidget {
+  const Fivew({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(width: MediaQuery.of(context).size.width * .05);
+  }
+}
